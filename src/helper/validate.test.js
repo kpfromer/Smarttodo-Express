f4918c839f8d9993c65deea3dd43dad2ec313894
Kyle Pfromer <kpfromer2@gmail.com>
2018-09-07 10:35:39 -0600

    Add if valid schema test to validate

--- src/helper/validate.test.js
+++ src/helper/validate.test.js
@@ -2,26 +2,36 @@ import joi from 'joi';
 import validate from './validate';
 
 describe('validate', () => {
-  let next;
+  let next, schema;
   beforeEach(() => {
     next = jest.fn();
-  });
-  it('handles error if data doesn\'t match joi schema', async () => {
-    const schema = joi.object().keys({
+    schema = joi.object().keys({
       name: joi.string().required(),
       age: joi.number().positive()
     }).required();
-
+  });
+  it('handles error if data doesn\'t match joi schema', async () => {
     const req = {
       body: {
         name: '',
         age: -12451235213
       }
     }
-
     await validate(schema)(req, undefined, next);
-
-    expect(next).toHaveBeenCalled();
     expect(next.mock.calls[0]).toMatchSnapshot();
+  });
+  it('attaches validated schema to req and continues express middleware chain', async () => {
+    const req = {
+      body: {
+        name: 'Jack Ryan',
+        age: 23
+      }
+    }
+    await validate(schema)(req, undefined, next);
+    expect(next).toHaveBeenCalledWith(); // called with nothing (like errors!)
+    expect(req.validatedBody).toEqual({
+      name: 'Jack Ryan',
+      age: 23
+    });
   });
 });
