2f30e96dc95fee66eb98efd7c38b90551d38e94c
Kyle Pfromer <kpfromer2@gmail.com>
2018-09-08 11:06:23 -0600

    Add mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import { error, notFound } from './modules/error/error';
+
+mongoose.connect('mongodb://localhost:27017/smarttodo-express', { useMongoClient: true });
 
 const app = express();
 // Express Config
