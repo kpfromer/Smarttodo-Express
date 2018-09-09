92ec7af8b7236ce165ad6bae9887e64e580c77c2
Kyle Pfromer <kpfromer2@gmail.com>
2018-09-09 11:02:09 -0600

    Add a config use convict

---
+++ src/config.js
@@ -0,0 +1,41 @@
+import convict from 'convict';
+
+// Define a schema
+const config = convict({
+  env: {
+    doc: "The application environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV"
+  },
+  port: {
+    doc: "The port for express to bind.",
+    format: "port",
+    default: 3000,
+    env: "PORT",
+    arg: "port"
+  },
+  jwt: {
+    secret: {
+      doc: "The jwt secret used to encode tokens",
+      format: String,
+      default: "jwt-dev-secret",
+      env: "JWT_SECRET"
+    },
+    expireIn: {
+      doc: "The time for the token to expire in",
+      format: [String, Number],
+      default: "1h",
+      env: "JWT_EXPIRE"
+    }
+  },
+  db: {
+    url: {
+      doc: "The mongo url used for connection.",
+      format: "*", // TODO: validate for mongo url
+      default: "mongodb://localhost:27017/smarttodo-express"
+    }
+  }
+});
+
+export default config;
